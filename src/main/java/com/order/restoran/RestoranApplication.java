f2effcfef848e320f7a0973393872fd50a7ef6ce
Yauhescha <evgescha.maslov007@yandex.ru>
2020-12-26 14:45:08 +0300

    Рабочий каркас

---
+++ src/main/java/com/order/restoran/RestoranApplication.java
@@ -0,0 +1,14 @@
+package com.order.restoran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoranApplication.class, args);
+		System.out.println("Application started");
+	}
+
+}
