fe06d3c6d0eb4e3d717e92911d64e5b74d6e0718
Yauhescha <evgescha.maslov007@yandex.ru>
2020-12-26 19:39:34 +0300

    добавлена работа с типом блюд

---
+++ src/main/java/com/order/restoran/controller/TypeFoodController.java
@@ -0,0 +1,56 @@
+package com.order.restoran.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.order.restoran.entity.TypeFood;
+import com.order.restoran.service.TypeFoodService;
+
+
+@Controller
+@RequestMapping("/typeFood")
+public class TypeFoodController {
+
+	@Autowired
+	TypeFoodService service;
+
+	@GetMapping
+	String getCategory(Model model) {
+		List<TypeFood> list = service.repository.findAll();
+		if (list == null)
+			model.addAttribute("list", null);
+		else
+			model.addAttribute("list", list);
+		return "typeFood-list";
+	}
+
+	@RequestMapping(path = { "/edit", "/edit/{id}" })
+	public String edit(Model model, @PathVariable(name = "id", required = false) Long id){
+		if (id != null) {
+			TypeFood entity = service.read(id);
+			model.addAttribute("entity", entity);
+		} else {
+			model.addAttribute("entity", new TypeFood());
+		}
+		return "typeFood-add-edit";
+	}
+
+	@RequestMapping(path = "/delete/{id}")
+	public String delete(Model model, @PathVariable("id") Long id) throws Exception {
+		service.delete(id);
+		return "redirect:/typeFood";
+	}
+
+	@RequestMapping(path = "/create", method = RequestMethod.POST)
+	public String createOrUpdate(TypeFood entity) throws Exception {
+		service.create(entity);
+		return "redirect:/typeFood";
+	}
+}
