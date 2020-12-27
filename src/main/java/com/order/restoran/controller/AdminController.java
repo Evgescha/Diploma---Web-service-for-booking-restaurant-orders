7d328fd41e995ff7ee88e51bbc650fbe1c0535f6
Yauhescha <evgescha.maslov007@yandex.ru>
2020-12-27 05:24:04 +0300

    закончена работа с отображением заказов

--- src/main/java/com/order/restoran/controller/AdminController.java
+++ src/main/java/com/order/restoran/controller/AdminController.java
@@ -1,16 +1,16 @@
 package com.order.restoran.controller;
 
-import java.security.Principal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.order.restoran.entity.User;
+import com.order.restoran.entity.Order;
 import com.order.restoran.service.FoodService;
 import com.order.restoran.service.OrderService;
+import com.order.restoran.service.StatusService;
 import com.order.restoran.service.TypeFoodService;
 import com.order.restoran.service.TypeTableService;
 import com.order.restoran.service.UserServiceImpl;
@@ -30,6 +30,10 @@ public class AdminController {
 
 	@Autowired
 	UserServiceImpl serviceUser;
+	@Autowired
+	OrderService serviceOrder;
+	@Autowired
+	StatusService serviceStatus;
 	
 	@GetMapping
 	@RequestMapping("/control")
@@ -39,5 +43,36 @@ public class AdminController {
 		model.addAttribute("typesTable",  serviceTypeTable.repository.findAll());
 		model.addAttribute("users",  serviceUser.repository.findAll());
 		return "control";
+	}
+	
+	@GetMapping
+	@RequestMapping("/booking")
+	String getBooking(Model model) {
+		model.addAttribute("list",  serviceOrder.repository.findAll());
+		return "booking";
+	}
+	
+	@GetMapping
+	@RequestMapping("/confirmOrder/{id}")
+	String confirmOrder(Model model,@PathVariable("id") Long id) {
+		Order order = serviceOrder.read(id);
+		order.setStatus(serviceStatus.read(3));
+		serviceOrder.update(order);
+		return "redirect:/admin/booking";
+	}
+
+	@GetMapping
+	@RequestMapping("/completeOrder/{id}")
+	String conpleteOrder(Model model,@PathVariable("id") Long id) {
+		Order order = serviceOrder.read(id);
+		order.setStatus(serviceStatus.read(4));
+		serviceOrder.update(order);
+		return "redirect:/admin/booking";
+	}
+	@GetMapping
+	@RequestMapping("/deleteOrder/{id}")
+	String deleteOrder(Model model,@PathVariable("id") Long id) throws Exception {
+		serviceOrder.delete(id);
+		return "redirect:/admin/booking";
 	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 INSERT IGNORE INTO role(id, name) VALUES(1,'ROLE_USER');
 INSERT IGNORE INTO role(id, name) VALUES(2,'ROLE_ADMIN');
 
-INSERT IGNORE INTO status(id, name) VALUES(1,'РЎРѕР·РґР°РµС‚СЃСЏ');
-INSERT IGNORE INTO status(id, name) VALUES(2,'РћР¶РёРґР°РµС‚ СЂРµС€РµРЅРёСЏ Р°РґРјРёРЅРёСЃС‚СЂР°С‚РѕСЂР°');
-INSERT IGNORE INTO status(id, name) VALUES(3,'РћРґРѕР±СЂРµРЅ');
-INSERT IGNORE INTO status(id, name) VALUES(4,'Р’С‹РїРѕР»РЅРµРЅ');
+INSERT IGNORE INTO status(id, name) VALUES(1,'В процессе создания');
+INSERT IGNORE INTO status(id, name) VALUES(2,'Ожидает решения администратора');
+INSERT IGNORE INTO status(id, name) VALUES(3,'Одобрен');
+INSERT IGNORE INTO status(id, name) VALUES(4,'Выполнен');
 
-INSERT IGNORE INTO type_table(id, name) VALUES(1,'Р¦РµРЅС‚СЂР°Р»СЊРЅС‹Р№ (РѕСЂР°РЅР¶РµРІС‹Р№)');
-INSERT IGNORE INTO type_table(id, name) VALUES(2,'Р”РІРѕР№РЅРѕР№ (С„РёРѕР»РµС‚РѕРІС‹Р№)');
-INSERT IGNORE INTO type_table(id, name) VALUES(3,'РЎ РґРёРІР°РЅРѕРј (СЃРµСЂС‹Р№)');
-INSERT IGNORE INTO type_table(id, name) VALUES(4,'РЎС‚СѓР»СЊРµРІРѕР№ (Р¶РµР»С‚С‹Р№)');
+INSERT IGNORE INTO type_table(id, name) VALUES(1,'Центральный (оранжевый)');
+INSERT IGNORE INTO type_table(id, name) VALUES(2,'Двойной (фиолетовый)');
+INSERT IGNORE INTO type_table(id, name) VALUES(3,'С диваном (серый)');
+INSERT IGNORE INTO type_table(id, name) VALUES(4,'Стульевой (желтый)');
 
 INSERT IGNORE INTO my_users(id, username, password,firstname,lastname,role_id) VALUES (1,'admin','$2a$10$7kQ1nv74qr7CiGAouEzxUOqoD9Pylh7nKY6WXDzAV6O1IF5R21tz.','admin','admin',2);
